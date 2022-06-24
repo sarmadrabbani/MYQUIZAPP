c4bb26da0984ca87748a17d28ecca88ef45140e5
win-10 <sarmadrabbani1313@gmail.com>
2022-06-24 11:08:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myquizapp/QuestionandAnswers.java
@@ -0,0 +1,29 @@
+package com.example.myquizapp;
+
+public class QuestionandAnswers {
+
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Where you are watching this video?",
+            "Which company owns the Apple?",
+            "What is always coming but never arrives? "
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"Toady ","Tomorrow","DayBeforeTomorrow","DayAfterTomorrow"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Apple",
+            "Tomorrow"
+    };
+
+}
